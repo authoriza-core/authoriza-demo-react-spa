8b552c27878cb43dcb8eb512d51a49cc5a236db1
Alex Kalinin <alexkalinin.ru@gmail.com>
2026-06-11 17:56:51 +0700

    typos

--- src/shared/lib/login.ts
+++ src/shared/lib/login.ts
@@ -3,7 +3,7 @@ import { generatePKCE, redirectToAuthorizaAuth } from '../../feature/auth';
 export async function login() {
   const { codeVerifier, codeChallenge } = await generatePKCE();
 
-  // сохраняем версификатор до callback
+  // сохраняем верификатор для callback
   sessionStorage.setItem("pkce_verifier", codeVerifier);
 
   redirectToAuthorizaAuth({
